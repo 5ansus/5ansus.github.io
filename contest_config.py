df9c4550b7251ed0035bd67cc5aefa745e3868c3
5ansus <santi02.fernandez.herrera@gmail.com>
2025-10-02 15:34:28 +0200

    Remake fork for make Ranking Website

---
+++ contest_config.py
@@ -0,0 +1,96 @@
+# Configuración de problemas del concurso
+# Basado en los problemas del index.html original
+CONTEST_PROBLEMS = [
+    {
+        'contest_id': 118,
+        'problem_index': 'A',
+        'name': 'Problem A',
+        'letter': 'A',
+        'order': 1
+    },
+    {
+        'contest_id': 4,
+        'problem_index': 'A',
+        'name': 'Problem B',
+        'letter': 'B',
+        'order': 2
+    },
+    {
+        'contest_id': 702,
+        'problem_index': 'A',
+        'name': 'Problem C',
+        'letter': 'C',
+        'order': 3
+    },
+    {
+        'contest_id': 2040,
+        'problem_index': 'A',
+        'name': 'Problem D',
+        'letter': 'D',
+        'order': 4
+    },
+    {
+        'contest_id': 2044,
+        'problem_index': 'A',
+        'name': 'Problem E',
+        'letter': 'E',
+        'order': 5
+    },
+    {
+        'contest_id': 2120,
+        'problem_index': 'A',
+        'name': 'Problem F',
+        'letter': 'F',
+        'order': 6
+    },
+    {
+        'contest_id': 1380,
+        'problem_index': 'A',
+        'name': 'Problem G',
+        'letter': 'G',
+        'order': 7
+    },
+    {
+        'contest_id': 1624,
+        'problem_index': 'C',
+        'name': 'Problem H',
+        'letter': 'H',
+        'order': 8
+    },
+    {
+        'contest_id': 115,
+        'problem_index': 'A',
+        'name': 'Problem I',
+        'letter': 'I',
+        'order': 9
+    },
+    {
+        'contest_id': 1739,
+        'problem_index': 'C',
+        'name': 'Problem J',
+        'letter': 'J',
+        'order': 10
+    },
+    {
+        'contest_id': 1845,
+        'problem_index': 'C',
+        'name': 'Problem K',
+        'letter': 'K',
+        'order': 11
+    },
+    {
+        'contest_id': 1994,
+        'problem_index': 'H',
+        'name': 'Problem L',
+        'letter': 'L',
+        'order': 12
+    }
+]
+
+# Configuración del concurso
+CONTEST_CONFIG = {
+    'name': 'Concurso Local: Intro 2025',
+    'start_time': '2025-10-02 11:00:00',  # Un poco antes de tu submission (21:22:01 local time)
+    'duration_hours': 3,  # Hasta las 00:00
+    'auto_update_interval': 60  # segundos
+}
